0b6c6f31f5ed8968f815c5ac755d94b8adb3fa6d
t3chTay <tayjohnson101@icloud.com>
2026-02-25 11:19:09 -0500

    inital commit, including flask_app.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,80 @@
-from flask import Flask, jsonify
+from flask import Flask
 from app.models import db
 from app.extensions import ma, limiter, cache
-from config import DevelopmentConfig
-from flask_swagger import swagger
-from flask_swagger_ui import get_swaggerui_blueprint
-from marshmallow.exceptions import ValidationError
+from config import DevelopmentConfig, ProductionConfig
+# from flask_swagger import swagger
+# from flask_swagger_ui import get_swaggerui_blueprint
+# from marshmallow.exceptions import ValidationError
+
+# def create_app(config_name=None):
+#     app = Flask(__name__)
+
+#     if config_name == "DevelopmentConfig":
+        
+#         app.config.from_object(DevelopmentConfig)
+
+#     db.init_app(app)
+#     ma.init_app(app)
+#     limiter.init_app(app)
+#     cache.init_app(app)
+    
+
+#     from app.blueprints.mechanic import mechanic_bp
+#     from app.blueprints.service_ticket import service_ticket_bp
+#     from app.blueprints.customers import customers_bp
+#     from app.blueprints.parts import parts_bp
+#     app.register_blueprint(mechanic_bp, url_prefix="/mechanics")
+#     app.register_blueprint(service_ticket_bp, url_prefix="/service-tickets")
+#     app.register_blueprint(customers_bp, url_prefix="/customers")
+#     app.register_blueprint(parts_bp, url_prefix="/parts")
+    
+#     @app.get("/swagger.json")
+#     def swagger_spec():
+#         swag = swagger(app)
+#         swag["info"]["title"] = "Auto Shop API"
+#         swag["info"]["version"] = "1.0"
+
+#         swag["securityDefinitions"] = {
+#             "BearerAuth": {
+#                 "type": "apiKey",
+#                 "name": "Authorization",
+#                 "in": "header",
+#                 "description": "Enter: Bearer <your_token>"
+#             }
+#         }
+
+#         return jsonify(swag)
+
+#     SWAGGER_URL = "/docs"
+#     API_URL = "/swagger.json"
+
+#     swagger_ui_blueprint = get_swaggerui_blueprint(
+#         SWAGGER_URL,
+#         API_URL,
+#         config={"app_name": "Auto Shop API"}
+#     )
+
+#     app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)        
+    
+#     @app.errorhandler(ValidationError)
+#     def handle_marshmallow_validation(err):
+#         return jsonify({"errors": err.messages}), 400
+    
+#     return app
+
 
 def create_app(config_name=None):
     app = Flask(__name__)
 
-    if config_name == "DevelopmentConfig":
-        
+    if config_name == "ProductionConfig":
+        app.config.from_object(ProductionConfig)
+    else:
         app.config.from_object(DevelopmentConfig)
 
     db.init_app(app)
     ma.init_app(app)
     limiter.init_app(app)
     cache.init_app(app)
-    
 
     from app.blueprints.mechanic import mechanic_bp
     from app.blueprints.service_ticket import service_ticket_bp
@@ -27,37 +84,5 @@ def create_app(config_name=None):
     app.register_blueprint(service_ticket_bp, url_prefix="/service-tickets")
     app.register_blueprint(customers_bp, url_prefix="/customers")
     app.register_blueprint(parts_bp, url_prefix="/parts")
-    
-    @app.get("/swagger.json")
-    def swagger_spec():
-        swag = swagger(app)
-        swag["info"]["title"] = "Auto Shop API"
-        swag["info"]["version"] = "1.0"
 
-        swag["securityDefinitions"] = {
-            "BearerAuth": {
-                "type": "apiKey",
-                "name": "Authorization",
-                "in": "header",
-                "description": "Enter: Bearer <your_token>"
-            }
-        }
-
-        return jsonify(swag)
-
-    SWAGGER_URL = "/docs"
-    API_URL = "/swagger.json"
-
-    swagger_ui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL,
-        API_URL,
-        config={"app_name": "Auto Shop API"}
-    )
-
-    app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)        
-    
-    @app.errorhandler(ValidationError)
-    def handle_marshmallow_validation(err):
-        return jsonify({"errors": err.messages}), 400
-    
     return app
--- config.py
+++ config.py
@@ -6,3 +6,12 @@ class DevelopmentConfig:
     
     CACHE_TYPE = "SimpleCache"
     CACHE_DEFAULT_TIMEOUT = 60
+    
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    DEBUG = False
+
+    CACHE_TYPE = "SimpleCache"
+    CACHE_DEFAULT_TIMEOUT = 60    
