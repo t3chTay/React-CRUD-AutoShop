6446c88349130de7662c5fd7e1da1e8fb3659d36
t3chTay <tayjohnson101@icloud.com>
2026-02-26 01:58:39 -0500

    remove duplicate init-db route

--- flask_app.py
+++ flask_app.py
@@ -2,8 +2,3 @@ from app import create_app
 from app.models import db
 
 app = create_app("ProductionConfig")
-
-@app.get("/init-db")
-def init_db():
-    db.create_all()
-    return {"message": "DB initialized"}, 200
